f086e0567113ffc8a5a335fa629535e41ca5b501
Alex Beng <599267065@qq.com>
2019-02-11 18:04:25 +0800

    integral version done

--- IntegralWithSlideWin.cpp
+++ IntegralWithSlideWin.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
-#include <opencv2/videoio.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
+#define POS_LABLE 1
+#define NEG_LABLE 0
+
 // VideoCapture打开的东西(string& filename/webcam index)
 #define CP_OPEN "/media/alex/Data/baseRelate/pic_data/frame%04d.jpg"
 
-cv::Mat GetUsedChannel(cv::Mat& image, int flag) {
+#define MODEL_NAME "../SvmTrain/ball_linear_auto.xml"
+
+#define IMG_COLS 32
+#define IMG_ROWS 32
+
+
+
+
+inline cv::Mat GetUsedChannel(cv::Mat& image, int flag) {
     cv::Mat hls_image;
     cv::Mat hsv_image;
     cv::Mat t_cs[3];
@@ -31,13 +38,62 @@ cv::Mat GetUsedChannel(cv::Mat& image, int flag) {
         cv::cvtColor(image, t_cs[0], CV_BGR2GRAY);
         return t_cs[0];
     }
+} 
+
+inline void Slide(cv::Mat& integral_image, std::vector<cv::Rect>& result, double thre) {
+    // define the wins size
+    std::vector<cv::Size> wins_sizes;
+    for (int i=10; i<=100; i+=10) {
+        wins_sizes.push_back(cv::Size(i,i));
+    }
+    int row = integral_image.rows;
+    int col = integral_image.cols;
+    
+    int row_step = 10;
+    int col_step = 10;
+    for (int k=0; k<wins_sizes.size(); k++) {
+        for (int i=0; i+wins_sizes[k].height<row; i+=row_step) {
+            for (int j=0; j+wins_sizes[k].width<col; j+=col_step) {
+                // compute ratio for thre
+                int win_sum = integral_image.at<int>(i+wins_sizes[k].height, j+wins_sizes[k].width) 
+                            + integral_image.at<int>(i, j)
+                            - integral_image.at<int>(i+wins_sizes[k].height, j)
+                            - integral_image.at<int>(i, j+wins_sizes[k].width);
+                if (1.0*win_sum/wins_sizes[k].area() > thre) {
+                    result.push_back(cv::Rect(cv::Point(j, i), wins_sizes[k]));
+                }    
+            }
+        }
+    }  
+}
+
+inline cv::Mat GetHogVec(cv::Mat& ROI) {
+    cv::resize(ROI, ROI, cv::Size(IMG_COLS, IMG_ROWS));
+    cv::HOGDescriptor hog_des(Size(IMG_COLS, IMG_ROWS), Size(16,16), Size(8,8), Size(8,8), 9);
+    std::vector<float> hog_vec;
+    hog_des.compute(ROI, hog_vec);
+
+    cv::Mat t(hog_vec);
+    cv::Mat hog_vec_in_mat = t.t();
+    hog_vec_in_mat.convertTo(hog_vec_in_mat, CV_32FC1);
+
+    return hog_vec_in_mat;
 }
 
 int main() {
+    // load SVM model
+#if CV_MAJOR_VERSION < 3
+    CvSVM tester;
+    tester.load(MODEL_NAME);
+#else
+    cv::Ptr<cv::ml::SVM> tester = cv::ml::SVM::load(MODEL_NAME);
+#endif
+
     cv::VideoCapture cp(CP_OPEN);
     cv::Mat frame;
     cv::Mat integral_frame;
     cv::Mat used_channel;
+    cv::Mat probable_pos;
     int used_channel_flag = 1;
 
     // thresholds
@@ -50,13 +106,18 @@ int main() {
     while (true) {
         cp >> frame;
         if (frame.empty()) {
-            cout<<"wait for cam init..."<<endl;
+            cout<<"wait for rebooting..."<<endl;
+            cp.open(CP_OPEN);
             continue;
         }
+#if CV_MAJOR_VERSION < 3
+        cv::flip(frame, frame, -1);
+        cv::resize(frame, frame, cv::Size(320, 240));
+#endif
         // blur 
         cv::GaussianBlur(frame, frame, cv::Size(5, 5), 0.);
 
-        // get used channel
+        // get used channel 
         used_channel = GetUsedChannel(frame, used_channel_flag);
 
          // thre 
@@ -64,13 +125,41 @@ int main() {
 
         // get intergral image
         cv::integral(thre_result, integral_frame, CV_32S);
-        cv::normalize(integral_frame, integral_frame, 0, 255, CV_MINMAX);
-        cv::convertScaleAbs(integral_frame, integral_frame);
+        integral_frame /= 255;
+
+        std::vector<cv::Rect> sld_result;
+        Slide(integral_frame, sld_result, 0.5);
+        
+        probable_pos = frame.clone();
+        for (auto i = sld_result.begin(); i != sld_result.end(); i++) {
+            cv::Mat t = frame(*i).clone();
+            cv::Mat hog_vec_in_mat = GetHogVec(t);
+#if CV_MAJOR_VERSION < 3
+            int lable = (int)tester.predict(hog_vec_in_mat);
+            if (lable == POS_LABLE) {
+                cv::rectangle(frame, *i, cv::Scalar(0, 255, 0), 2);
+            }
+            else {
+                cv::rectangle(frame, *i, cv::Scalar(0, 0, 255), 2);
+            }
+#else
+            cv::Mat lable;
+            tester->predict(hog_vec_in_mat, lable);
+            if (lable.at<float>(0, 0) == POS_LABLE) {
+                cv::rectangle(frame, *i, cv::Scalar(0, 255, 0), 2);
+            } 
+            else {
+                cv::rectangle(frame, *i, cv::Scalar(0, 0, 255), 2);
+            }
+#endif
+        }
+
 
         cv::imshow("living", frame);
         cv::imshow("thre", thre_result);
         cv::imshow("integral", integral_frame);
-        char key = cv::waitKey(0);
+        cv::imshow("sld_result", probable_pos);
+        char key = cv::waitKey(1);
         if (key == 'q') {
             break;
         }
--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
+rm ./demo.out
+command="g++ IntegralWithSlideWin.cpp `pkg-config --cflags --libs opencv` -std=c++11 -o demo.out"
+result=`$command`
+if [ ! -f "demo.out" ];
+then 
+    echo "compile error"
+else   
+    echo "build succeed"
+    ./demo.out
+fi
 # g++ main.cpp ../HogGetter/HogGetter.cpp -o demo -std=c++11 -I../HogGetter/ -IC:\\opencv\\build\\include -LC:\\opencv\\build\\bin -llibopencv_calib3d249 -llibopencv_contrib249 -llibopencv_core249 -llibopencv_features2d249 -llibopencv_flann249 -llibopencv_gpu249 -llibopencv_highgui249 -llibopencv_imgproc249 -llibopencv_legacy249 -llibopencv_ml249 -llibopencv_nonfree249 -llibopencv_objdetect249 -llibopencv_ocl249 -llibopencv_photo249 -llibopencv_stitching249 -llibopencv_superres249 -llibopencv_video249 -llibopencv_videostab249
 # g++ HlsThreWithMaxContours.cpp `pkg-config --cflags --libs opencv` -std=c++11 -o demo.out
 # g++ Blob.cpp `pkg-config --cflags --libs opencv` -std=c++11 -o demo.out
-g++ IntegralWithSlideWin.cpp `pkg-config --cflags --libs opencv` -std=c++11 -o demo.out
